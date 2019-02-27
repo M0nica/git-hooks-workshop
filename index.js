0f8831241b458f1c9f78b50ee2f013c6fdbdae2c
Monica Powell <mpowell@meetup.com>
2019-02-27 15:59:49 -0500

    comment out file that makes eslint angry

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // bad styling that should fail eslint
 
-const nameChecker = function (name) {
-  if (name == 'Cardi') {
-    console.log('Party with Cardi');
-  }
+// const nameChecker = function (name) {
+//   if (name == 'Cardi') {
+//     console.log('Party with Cardi');
+//   }
 
-  function doSomething() {
-    console.log('do something');
-  }
-};
+//   function doSomething() {
+//     console.log('do something');
+//   }
+// };
